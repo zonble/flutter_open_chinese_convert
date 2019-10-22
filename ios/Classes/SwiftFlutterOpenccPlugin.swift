2e1c7c61883e94a9427d341a2ff5179e5f2ce5d9
zonble <zonble@gmail.com>
2019-10-23 00:28:50 +0800

    Implements the plugin on iOS.

--- ios/Classes/SwiftFlutterOpenccPlugin.swift
+++ ios/Classes/SwiftFlutterOpenccPlugin.swift
@@ -1,14 +1,50 @@
 import Flutter
 import UIKit
+import OpenCC
 
 public class SwiftFlutterOpenccPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_opencc", binaryMessenger: registrar.messenger())
-    let instance = SwiftFlutterOpenccPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+	public static func register(with registrar: FlutterPluginRegistrar) {
+		let channel = FlutterMethodChannel(name: "flutter_opencc", binaryMessenger: registrar.messenger())
+		let instance = SwiftFlutterOpenccPlugin()
+		registrar.addMethodCallDelegate(instance, channel: channel)
+	}
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+	func convertOption(from: String) -> ChineseConverter.Options {
+		let map: [String: ChineseConverter.Options] = [
+			"s2t": [.traditionalize],
+			"t2s": [.simplify],
+			"s2hk": [.traditionalize, .HKStandard],
+			"hk2s": [.simplify, .HKStandard],
+			"s2tw": [.traditionalize, .TWStandard],
+			"tw2s": [.simplify, .TWStandard],
+			"s2twp": [.traditionalize, .TWStandard, .TWIdiom],
+			"tw2sp": [.simplify, .TWStandard, .TWIdiom],
+		]
+		return map[from] ?? []
+	}
+
+	public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+		let method = call.method
+		switch method {
+		case "convert":
+			guard let list = call.arguments as? [String],
+				list.count >= 2 else {
+				return
+			}
+			let text = list[0]
+			let optionString = list[1]
+			do {
+				let converter = try ChineseConverter(option: convertOption(from: optionString))
+				let converted = converter.convert(text)
+				result(converted)
+			} catch {
+				let flutterError = FlutterError(code: "0", message: error.localizedDescription, details: nil)
+				result(flutterError)
+			}
+		default:
+			let flutterError = FlutterError(code: "1", message: "Not supported", details: nil)
+			result(flutterError)
+
+		}
+	}
 }
--- ios/flutter_opencc.podspec
+++ ios/flutter_opencc.podspec
@@ -2,20 +2,23 @@
 # To learn more about a Podspec see http://guides.cocoapods.org/syntax/podspec.html
 #
 Pod::Spec.new do |s|
-  s.name             = 'flutter_opencc'
-  s.version          = '0.0.1'
-  s.summary          = 'A new Flutter project.'
-  s.description      = <<-DESC
+  s.name = "flutter_opencc"
+  s.version = "0.0.1"
+  s.summary = "A new Flutter project."
+  s.description = <<-DESC
 A new Flutter project.
                        DESC
-  s.homepage         = 'http://example.com'
-  s.license          = { :file => '../LICENSE' }
-  s.author           = { 'Your Company' => 'email@example.com' }
-  s.source           = { :path => '.' }
-  s.source_files = 'Classes/**/*'
-  s.public_header_files = 'Classes/**/*.h'
-  s.dependency 'Flutter'
-
-  s.ios.deployment_target = '8.0'
+  s.homepage = "http://example.com"
+  s.license = { :file => "../LICENSE" }
+  s.author = { "Your Company" => "email@example.com" }
+  s.source = { :path => "." }
+  s.source_files = "Classes/**/*"
+  s.public_header_files = "Classes/**/*.h"
+  s.dependency "Flutter"
+  s.dependency "OpenCC"
+  s.xcconfig = {
+    "HEADER_SEARCH_PATHS" => "$(PODS_ROOT)/OpenCC/SwiftOpenCC/Sources/OpenCCBridge/include",
+    "OTHER_CFLAGS" => "-fembed-bitcode",
+  }
+  s.ios.deployment_target = "8.0"
 end
-
