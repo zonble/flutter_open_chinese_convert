0e349ff62dbb72e5eaaee196c27da16ae807231f
zonble <zonble@gmail.com>
2019-10-23 10:56:21 +0800

    Fixes the build.

--- android/src/main/kotlin/net/zonble/flutter_opencc/FlutterOpenccPlugin.kt
+++ android/src/main/kotlin/net/zonble/flutter_opencc/FlutterOpenccPlugin.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_opencc
+package net.zonble.flutter_opencc
 
 import android.content.Context
 import com.zqc.opencc.android.lib.ChineseConverter
@@ -10,46 +10,46 @@ import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class FlutterOpenccPlugin(var context: Context) : MethodCallHandler {
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val context = registrar.activeContext()
-            val channel = MethodChannel(registrar.messenger(), "flutter_opencc")
-            channel.setMethodCallHandler(FlutterOpenccPlugin(context))
-        }
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar) {
+      val context = registrar.activeContext()
+      val channel = MethodChannel(registrar.messenger(), "flutter_opencc")
+      channel.setMethodCallHandler(FlutterOpenccPlugin(context))
     }
+  }
 
-    fun type(option: String): ConversionType? {
-        return when (option) {
-            "s2t" -> ConversionType.S2T
-            "t2s" -> ConversionType.T2S
-            "s2hk" -> ConversionType.S2HK
-            "hk2s" -> ConversionType.HK2S
-            "s2tw" -> ConversionType.S2TW
-            "tw2s" -> ConversionType.TW2S
-            "s2twp" -> ConversionType.S2TWP
-            "tw2sp" -> ConversionType.TW2SP
-            "t2tw" -> ConversionType.T2TW
-            "t2hk" -> ConversionType.T2HK
-            else -> null
-        }
+  fun type(option: String): ConversionType? {
+    return when (option) {
+      "s2t" -> ConversionType.S2T
+      "t2s" -> ConversionType.T2S
+      "s2hk" -> ConversionType.S2HK
+      "hk2s" -> ConversionType.HK2S
+      "s2tw" -> ConversionType.S2TW
+      "tw2s" -> ConversionType.TW2S
+      "s2twp" -> ConversionType.S2TWP
+      "tw2sp" -> ConversionType.TW2SP
+      "t2tw" -> ConversionType.T2TW
+      "t2hk" -> ConversionType.T2HK
+      else -> null
     }
+  }
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        when (call.method) {
-            "convert" -> {
-                val arguments = call.arguments as? ArrayList<*>
-                arguments?.also {
-                    val text = it[0] as String
-                    val optionString = it[1] as String
-                    val option = type(optionString)
-                    option?.let {
-                        val converted = ChineseConverter.convert(text, it, this.context)
-                        result.success(converted)
-                    } ?: result.error("Not supported", null, null)
-                }
-            }
-            else -> result.notImplemented()
+  override fun onMethodCall(call: MethodCall, result: Result) {
+    when (call.method) {
+      "convert" -> {
+        val arguments = call.arguments as? ArrayList<*>
+        arguments?.also {
+          val text = it[0] as String
+          val optionString = it[1] as String
+          val option = type(optionString)
+          option?.let {
+            val converted = ChineseConverter.convert(text, it, this.context)
+            result.success(converted)
+          } ?: result.error("Not supported", null, null)
         }
+      }
+      else -> result.notImplemented()
     }
+  }
 }
