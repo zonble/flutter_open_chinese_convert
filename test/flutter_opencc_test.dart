9ff3e2b300fe66ed8c2245ef8804ec4329171cfd
zonble <zonble@gmail.com>
2024-08-27 17:52:14 +0800

    Sorts import.

--- test/flutter_opencc_test.dart
+++ test/flutter_opencc_test.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_open_chinese_convert/flutter_open_chinese_convert.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+
+import 'package:flutter_open_chinese_convert/flutter_open_chinese_convert.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_open_chinese_convert');
