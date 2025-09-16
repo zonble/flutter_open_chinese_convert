6cf454a0aa46aa53746970abee70b78cea6c61d5
zonble <zonble@gmail.com>
2025-09-16 17:26:40 +0800

    Sets minSdk to 24
    Sets the minimum SDK version to 24.
    
    This change overrides the default Flutter minimum SDK version
    to ensure compatibility with certain dependencies or features
    used in the application.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -24,7 +24,8 @@ android {
         applicationId = "net.zonble.flutter_open_chinese_convert_example"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        // minSdk = flutter.minSdkVersion
+        minSdk = 24
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
